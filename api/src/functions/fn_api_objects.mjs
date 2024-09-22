b73ad7508e2d522bc7826596b980bf69604b4421
Pierre Constantineau <jpconstantineau@gmail.com>
2024-09-21 22:25:19 -0600

    updated api for objects

--- api/src/functions/fn_api_objects.mjs
+++ api/src/functions/fn_api_objects.mjs
@@ -24,7 +24,7 @@ app.http('item', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     route: 'item/{UserID:minlength(4)}/{ObjectType:minlength(4)}/{ObjectID:minlength(4)}',
-    extraInputs: [cosmosInput],
+  //  extraInputs: [cosmosInput],
   //  extraOutputs: [sendToCosmosDb],
     handler: (request, context) => {
 
