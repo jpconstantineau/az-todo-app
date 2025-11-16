585a9232a4ef17deab9ea7b9aacb01df29916603
Pierre Constantineau <pierre.constantineau@corrology.com>
2025-11-16 16:40:45 -0600

    updated api

--- api/api/shared/db.mjs
+++ api/api/shared/db.mjs
@@ -1,16 +1,20 @@
 // api/shared/db.mjs
 import { CosmosClient } from "@azure/cosmos";
 
-const endpoint = process.env.COSMOS_ENDPOINT;
-const key = process.env.COSMOS_KEY;
-const databaseId = process.env.COSMOS_DB || "gtd";
-const containerId = process.env.COSMOS_CONTAINER || "gtd";
+//const endpoint = process.env.COSMOS_ENDPOINT;
+//const key = process.env.COSMOS_KEY;
+//if (!endpoint || !key) {
+//  console.warn("COSMOS_ENDPOINT/COSMOS_KEY not set");
+//}
+//const client = new CosmosClient({ endpoint, key });
 
-if (!endpoint || !key) {
-  console.warn("COSMOS_ENDPOINT/COSMOS_KEY not set");
-}
+const databaseId = process.env.COSMOS_DB || "ToDoList";
+const containerId = process.env.COSMOS_CONTAINER || "Items";
 
-const client = new CosmosClient({ endpoint, key });
+const CosmosDbConnectionSetting = process.env.CosmosDbConnectionSetting;
+
+const client = new CosmosClient(CosmosDbConnectionSetting);
+
 const db = client.database(databaseId);
 const container = db.container(containerId);
 
